6edd97adffd9bf934b49cc8a0ebc1b33f20a9813
alpacamale <alpaca9100@gmail.com>
2025-11-10 23:38:43 +0900

    Initial commit

--- section_2/task.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator!")
-total_bill = int(input("What was the total bill? $"))
-tip_rate = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill? "))
-print(
-    "Each person should pay: "
-    + str((total_bill + (total_bill * tip_rate / 100)) / people)
-)
